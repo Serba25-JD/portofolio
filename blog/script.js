698eb4172eaa77ecd524d2788a178197f63e7dce
Serba25-JD <hydebb.99@gmail.com>
2025-11-14 16:58:37 +0700

    update

--- blog/script.js
+++ blog/script.js
@@ -62,6 +62,7 @@ function showSelfPaced() {
         { 'title': 'Pertemuan 4', 'id': 'eye-keempat', 'judul': 'Analisis Yuridis Kasus Korupsi Digital di Indonesia.'},
         { 'title': 'Pertemuan 5', 'id': 'eye-kelima', 'judul': 'Tugas 1: Analisis Dilema Etika. Tugas 2: Diskusi Kelompok.' },
         { 'title': 'Pertemuan 6', 'id': 'eye-keenam', 'judul': 'Tugas 1: Analisis Kasus Etika Teknologi. Tugas 2: Diskusi topik.'},
+        { 'title': 'Pertemuan 7', 'id': 'eye-ketujuh', 'judul': 'Menegakkan Integritas Digital di Era AI dan Disinformasi.'},
     ]
     listData.forEach(data => {
         const divContent = document.createElement('div');
@@ -73,6 +74,7 @@ function showSelfPaced() {
         icon.setAttribute('data-feather', 'eye');
         icon.setAttribute('id', data.id);
         icon.classList.add('eye-icon');
+        icon.classList.add('eye-icon');
         divContent.append(h2, p, icon);
         divContainer.appendChild(divContent);
     });
@@ -81,192 +83,126 @@ function showSelfPaced() {
     showDocument();
 }
 
-function showDocument() {
-    documentFirst();
-    documentTwo();
-    documentThree();
-    documentFour();
-    documentFive();
-    documentSix();
-}
-
-function documentFirst() {
-    const pertama = document.getElementById('eye-pertama');
-    pertama.addEventListener('click', function() {
-        const container = document.getElementById('container-self-paced');
-        const divContainer = document.createElement('div');
-        divContainer.classList.add('container-popup');
-        divContainer.setAttribute('id', 'container-popup');
-        divContainer.style.display = 'flex';
-        const divContent = document.createElement('div');
-        divContent.classList.add('content-popup');
-        const iframe = document.createElement('iframe');
-        iframe.src = 'https://drive.google.com/file/d/1qx4orcWGXiYoDlBIYE8zP5uUF4OvLPfo/preview';
-        iframe.width = '100%';
-        iframe.height = '800px';
-        const button = document.createElement('button');
-        button.type = 'reset';
-        button.textContent = 'Tutup';
-        button.setAttribute('id', 'close-btn');
-        divContent.append(iframe, button);
-        divContainer.appendChild(divContent);
-        container.insertAdjacentElement('beforeend', divContainer);
-        closePopup();
-    })
-}
-
-function documentTwo() {
-    const kedua = document.getElementById('eye-kedua');
-    kedua.addEventListener('click', function() {
-        const container = document.getElementById('container-self-paced');
-        const divContainer = document.createElement('div');
-        divContainer.classList.add('container-popup');
-        divContainer.setAttribute('id', 'container-popup');
-        divContainer.style.display = 'flex';
-        const divContent = document.createElement('div');
-        divContent.classList.add('content-popup');
-        const iframe = document.createElement('iframe');
-        iframe.src = 'https://drive.google.com/file/d/1Cs4P_d1Glc6gFCdac67SbB3iJ5Z3cnsh/preview';
-        iframe.width = '100%';
-        iframe.height = '800px';
-        const button = document.createElement('button');
-        button.type = 'reset';
-        button.textContent = 'Tutup';
-        button.setAttribute('id', 'close-btn');
-        divContent.append(iframe, button);
-        divContainer.appendChild(divContent);
-        container.insertAdjacentElement('beforeend', divContainer);
-        closePopup();
-    })
-}
-
-function documentThree() {
-    const ketiga = document.getElementById('eye-ketiga');
-    ketiga.addEventListener('click', function() {
-        const container = document.getElementById('container-self-paced');
-        const divContainer = document.createElement('div');
-        divContainer.classList.add('container-popup');
-        divContainer.setAttribute('id', 'container-popup');
-        divContainer.style.display = 'flex';
-        const divContent = document.createElement('div');
-        divContent.classList.add('content-popup');
-        const iframe = document.createElement('iframe');
-        iframe.src = 'https://docs.google.com/presentation/d/e/2PACX-1vQbVCJpzI2nUbZFRprQOxS_FpNQsb_MKIUZA5iFgJzEsJfLqHfRF2f8i7hop8Vi4A/pubembed?start=true&loop=true&delayms=3000';
-        iframe.width = '1280';
-        iframe.height = '749';
-        iframe.allowfullscreen = 'true';
-        iframe.frameborder = '0';
-        iframe.mozallowfullscreen = 'true';
-        iframe.webkitallowfullscreen = 'true';
-        const button = document.createElement('button');
-        button.type = 'reset';
-        button.textContent = 'Tutup';
-        button.setAttribute('id', 'close-btn');
-        divContent.append(iframe, button);
-        divContainer.appendChild(divContent);
-        container.insertAdjacentElement('beforeend', divContainer);
-        closePopup();
-    })
-}
-
-function documentFour() {
-    const keempat = document.getElementById('eye-keempat');
-    keempat.addEventListener('click', function() {
-        const container = document.getElementById('container-self-paced');
-        const divContainer = document.createElement('div');
-        divContainer.classList.add('container-popup');
-        divContainer.setAttribute('id', 'container-popup');
-        divContainer.style.display = 'flex';
-        const divContent = document.createElement('div');
-        divContent.classList.add('content-popup');
-        const iframe = document.createElement('iframe');
-        iframe.src = 'https://drive.google.com/file/d/125yam0QpIghaPTiotOoI3e66QkW8-xwr/preview';
-        iframe.width = '100%';
-        iframe.height = '800px';
-        const button = document.createElement('button');
-        button.type = 'reset';
-        button.textContent = 'Tutup';
-        button.setAttribute('id', 'close-btn');
-        divContent.append(iframe, button);
-        divContainer.appendChild(divContent);
-        container.insertAdjacentElement('beforeend', divContainer);
-        closePopup();
-    })
-}
-
-function documentFive() {
-    const kelima = document.getElementById('eye-kelima');
-    kelima.addEventListener('click', function() {
-        const container = document.getElementById('container-self-paced');
-        const divContainer = document.createElement('div');
-        divContainer.classList.add('container-popup');
-        divContainer.setAttribute('id', 'container-popup');
-        divContainer.style.display = 'flex';
-        const divContent = document.createElement('div');
-        divContent.classList.add('content-popup');
-        const divDocumentContainer = document.createElement('div');
-        divDocumentContainer.classList.add('eye-content');
-        const listDocument = [
-            { 'judul': 'Analisis Dilema Etika', 'href': 'https://drive.google.com/file/d/1iy2IP1CUVNrEq4NZW7EOJ4ZwKZEyH5AN/preview'},
-            { 'judul': 'Apakah whistleblowing atau tindakan melaporkan atau membocorkan dugaan pelanggaran atau kecurangan yang terjadi di dalam suatu organisasi di lingkungan kerja merupakan tindakan etis?.', 'href': 'https://docs.google.com/presentation/d/e/2PACX-1vTDeynEODbSEQwAS2qAfudO92TR7wcyMjx-Qh3-oJslq2hCmLz6pPM9JdAkN7JjyQ/pubembed?start=true&loop=true&delayms=3000' }
+const popupData = [
+    {
+        id: 'eye-pertama',
+        type: 'iframe',
+        documents: [
+            { src: 'https://drive.google.com/file/d/1qx4orcWGXiYoDlBIYE8zP5uUF4OvLPfo/preview' }
         ]
-        listDocument.forEach(list => {
-            const divDocumentContent = document.createElement('div');
+    },
+    {
+        id: 'eye-kedua',
+        type: 'iframe',
+        documents: [
+            { src: 'https://drive.google.com/file/d/1Cs4P_d1Glc6gFCdac67SbB3iJ5Z3cnsh/preview' }
+        ]
+    },
+    {
+        id: 'eye-ketiga',
+        type: 'iframe',
+        documents: [
+            { src: 'https://docs.google.com/presentation/d/e/2PACX-1vQbVCJpzI2nUbZFRprQOxS_FpNQsb_MKIUZA5iFgJzEsJfLqHfRF2f8i7hop8Vi4A/pubembed?start=true&loop=true&delayms=3000' }
+        ]
+
+    },
+    {
+        id: 'eye-keempat',
+        type: 'iframe',
+        documents: [
+            { src: 'https://drive.google.com/file/d/125yam0QpIghaPTiotOoI3e66QkW8-xwr/preview' }
+        ]
+
+    },
+    {
+        id: 'eye-kelima',
+        type: 'list',
+        documents: [
+            { 
+                judul: 'Analisis Dilema Etika', 
+                href: 'https://drive.google.com/file/d/1iy2IP1CUVNrEq4NZW7EOJ4ZwKZEyH5AN/preview' 
+            },
+            { 
+                judul: 'Apakah whistleblowing atau tindakan melaporkan atau membocorkan dugaan pelanggaran atau kecurangan yang terjadi di dalam suatu organisasi di lingkungan kerja merupakan tindakan etis.', 
+                href: 'https://docs.google.com/presentation/d/e/2PACX-1vTDeynEODbSEQwAS2qAfudO92TR7wcyMjx-Qh3-oJslq2hCmLz6pPM9JdAkN7JjyQ/pubembed?start=true&loop=true&delayms=3000' 
+            }
+        ]
+    },
+    {
+        id: 'eye-keenam',
+        type: 'list',
+        documents: [
+            { 
+                judul: 'Analisis Kasus Etika Teknologi', 
+                href: 'https://drive.google.com/file/d/1b4RQK8VGJ_1ddovVDtbie6pjGR1URmRe/preview' 
+            },
+            { 
+                judul: 'Apakah keamanan siber merupakan tanggung jawab moral atau teknis? Mahasiswa membuat argumentasi berdasarkan keempat prinsip etika TI.', 
+                href: 'https://drive.google.com/file/d/1j49yi4k1RKmj9aI9sh_esIrmf6HtEAI6/preview' 
+            }
+        ]
+    },
+    {
+        id: 'eye-ketujuh',
+        type: 'iframe',
+        documents: [
+            { src: 'https://drive.google.com/file/d/1JmnNDh3sw_EaYJb3Jznb3eFMSf6b0nfv/preview' }
+        ]
+    }
+];
+
+function createContainer(data) {
+    const container = document.getElementById('container-self-paced');
+    const divContainer = document.createElement('div');
+    divContainer.classList.add('container-popup');
+    divContainer.id = 'container-popup';
+    divContainer.style.display = 'flex';
+    const divContent = document.createElement('div');
+    divContent.classList.add('content-popup');
+    if (data.type === 'iframe' && data.documents.length === 1) {
+        const iframe = document.createElement('iframe');
+        iframe.src = data.documents[0].src;
+        iframe.width = '100%';
+        iframe.height = '800px';
+        divContent.appendChild(iframe);
+    }
+    else if (data.type === 'list' || data.documents.length > 1) {
+        const divList = document.createElement('div');
+        divList.classList.add('eye-content');
+
+        data.documents.forEach(doc => {
+            const divDoc = document.createElement('div');
             const h3 = document.createElement('h3');
-            h3.textContent = list.judul;
+            h3.textContent = doc.judul;
             const a = document.createElement('a');
-            a.href = list.href;
+            a.href = doc.href;
             a.target = '_blank';
             a.textContent = 'Lihat';
-            divDocumentContent.append(h3, a);
-            divDocumentContainer.appendChild(divDocumentContent);
-        })
-        const button = document.createElement('button');
-        button.type = 'reset';
-        button.textContent = 'Tutup';
-        button.setAttribute('id', 'close-btn');
-        divContent.append(divDocumentContainer, button);
-        divContainer.appendChild(divContent);
-        container.insertAdjacentElement('beforeend', divContainer);
-        closePopup();
-    })
+            divDoc.append(h3, a);
+            divList.appendChild(divDoc);
+        });
+        divContent.appendChild(divList);
+    }
+    const button = document.createElement('button');
+    button.type = 'reset';
+    button.textContent = 'Tutup';
+    button.id = 'close-btn';
+    divContent.appendChild(button);
+    divContainer.appendChild(divContent);
+    container.appendChild(divContainer);
+    closePopup();
 }
 
-function documentSix() {
-    const keenam = document.getElementById('eye-keenam');
-    keenam.addEventListener('click', function() {
-        const container = document.getElementById('container-self-paced');
-        const divContainer = document.createElement('div');
-        divContainer.classList.add('container-popup');
-        divContainer.setAttribute('id', 'container-popup');
-        divContainer.style.display = 'flex';
-        const divContent = document.createElement('div');
-        divContent.classList.add('content-popup');
-        const divDocumentContainer = document.createElement('div');
-        divDocumentContainer.classList.add('eye-content');
-        const listDocument = [
-            { 'judul': 'Analisis Kasus Etika Teknologi', 'href': 'https://drive.google.com/file/d/1b4RQK8VGJ_1ddovVDtbie6pjGR1URmRe/preview'},
-            { 'judul': 'Apakah keamanan siber merupakan tanggung jawab moral atau teknis? Mahasiswa membuat argumentasi berdasarkan keempat prinsip etika TI.', 'href': 'https://drive.google.com/file/d/1j49yi4k1RKmj9aI9sh_esIrmf6HtEAI6/preview' }
-        ]
-        listDocument.forEach(list => {
-            const divDocumentContent = document.createElement('div');
-            const h3 = document.createElement('h3');
-            h3.textContent = list.judul;
-            const a = document.createElement('a');
-            a.href = list.href;
-            a.textContent = 'Check Disini';
-            a.target = '_blank';
-            divDocumentContent.append(h3, a);
-            divDocumentContainer.appendChild(divDocumentContent);
+function showDocument() {
+    const allEye = document.querySelectorAll('.eye-icon');
+    allEye.forEach(eye => {
+        eye.addEventListener('click', function(e) {
+            const target = e.target.id;
+            const selected = popupData.find(item => item.id === target);
+            if (selected) {
+                createContainer(selected);
+            }
         })
-        const button = document.createElement('button');
-        button.type = 'reset';
-        button.textContent = 'Tutup';
-        button.setAttribute('id', 'close-btn');
-        divContent.append(divDocumentContainer, button);
-        divContainer.appendChild(divContent);
-        container.insertAdjacentElement('beforeend', divContainer);
-        closePopup();
     })
 }
 
@@ -316,7 +252,7 @@ function showPopup() {
 }
 
 function changeCache() {
-    const version = '20251106';
+    const version = '20251114';
     document.querySelectorAll('link[rel="stylesheet"]').forEach(link => {
     link.href = link.href + '?v=' + version;
     });
