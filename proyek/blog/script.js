95c3e6a56b0ac758fe332db6ad942333b7afd52a
Serba25-JD <hydebb.99@gmail.com>
2025-12-04 17:03:52 +0700

    update

--- proyek/blog/script.js
+++ proyek/blog/script.js
@@ -1,276 +1,48 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // const favicon = document.createElement('link');
-    // favicon.rel = 'icon';
-    // favicon.href = '../../assets/image/main/icon.ico';
-    // favicon.sizes = '48x48';
-    // const style = document.createElement('link');
-    // style.rel = 'stylesheet';
-    // style.href = 'styles.css';
-    // document.head.append(favicon, style);
-    changeCache();
     showLogin();
+    changeCache();
 });
 
-function showLayout() {
-    const header = document.createElement('header');
-    header.setAttribute('id', 'header-container');
-    const main = document.createElement('main');
-    main.setAttribute('id', 'main-container');
-    const footer = document.createElement('footer');
-    footer.setAttribute('id', 'footer-container');
-    document.body.append(header, main, footer);
-    showLoadLayout();
-}
-
-function showLoadLayout() {
-    showHeader();
-    showMain();
-    showFooter();
-}
-
-function showHeader() {
-    const header = document.getElementById('header-container');
-    const h1 = document.createElement('h1');
-    h1.textContent = 'Selamat datang di blog saya';
-    header.appendChild(h1);
-}
-
-function showMain() {
-    const main = document.getElementById('main-container');
-    const dataID = [
-        { 'id': 'container-self-paced'},
-    ]
-    dataID.forEach(data => {
-        const section = document.createElement('section');
-        section.classList.add('container');
-        section.setAttribute('id', data.id);
-        main.appendChild(section);
-    });
-    showSelfPaced();
-}
-
-function showSelfPaced() {
-    const main = document.getElementById('container-self-paced');
-    const divTitle = document.createElement('div');
-    const h2Title = document.createElement('h2');
-    h2Title.textContent = 'Pendidikan Anti Korupsi dan Etika Berteknologi';
-    divTitle.appendChild(h2Title);
-    main.appendChild(divTitle);
-    const divContainer = document.createElement('div');
-    divContainer.classList.add('self-paced-container');
-    const listData = [
-        { 'title': 'Pertemuan 1', 'id':'eye-pertama', 'judul': 'Bagaimana Anda memaknai kejujuran dan tanggung jawab dalam penggunaan teknologi digital di kehidupan sehari-hari ?'},
-        { 'title': 'Pertemuan 2', 'id': 'eye-kedua', 'judul': 'Analisis Etika dan Teknologi dalam kasus Korupsi Digital di Indonesia.'},
-        { 'title': 'Pertemuan 3', 'id': 'eye-ketiga', 'judul': 'Merancang Pedoman Etika Digital di Lingkungan Kampus.'},
-        { 'title': 'Pertemuan 4', 'id': 'eye-keempat', 'judul': 'Analisis Yuridis Kasus Korupsi Digital di Indonesia.'},
-        { 'title': 'Pertemuan 5', 'id': 'eye-kelima', 'judul': 'Tugas 1: Analisis Dilema Etika. Tugas 2: Diskusi Kelompok.' },
-        { 'title': 'Pertemuan 6', 'id': 'eye-keenam', 'judul': 'Tugas 1: Analisis Kasus Etika Teknologi. Tugas 2: Diskusi topik.'},
-        { 'title': 'Pertemuan 7', 'id': 'eye-ketujuh', 'judul': 'Menegakkan Integritas Digital di Era AI dan Disinformasi.'},
-        { 'title': 'Pertemuan 8', 'id': 'eye-kedelapan', 'judul': 'Ujian Tengah Semester.'},
-        { 'title': 'Pertemuan 9', 'id': 'eye-kesembilan', 'judul': 'Tugas 1: Analisis Kasus Korupsi Digital. Tugas 2: Tugas Kelompok: Simulasi Tender Etis.'},
-    ]
-    listData.forEach(data => {
-        const divContent = document.createElement('div');
-        const h2 = document.createElement('h2');
-        h2.textContent = data.title;
-        const p = document.createElement('p');
-        p.textContent = data.judul;
-        const icon = document.createElement('i');
-        icon.setAttribute('data-feather', 'eye');
-        icon.setAttribute('id', data.id);
-        icon.classList.add('eye-icon');
-        divContent.append(h2, p, icon);
-        divContainer.appendChild(divContent);
-    });
-    main.appendChild(divContainer);
-    feather.replace();
-    showDocument();
-}
-
-const popupData = [
-    {
-        id: 'eye-pertama',
-        type: 'iframe',
-        documents: [
-            { src: 'https://drive.google.com/file/d/1qx4orcWGXiYoDlBIYE8zP5uUF4OvLPfo/preview' }
-        ]
-    },
-    {
-        id: 'eye-kedua',
-        type: 'iframe',
-        documents: [
-            { src: 'https://drive.google.com/file/d/1Cs4P_d1Glc6gFCdac67SbB3iJ5Z3cnsh/preview' }
-        ]
-    },
-    {
-        id: 'eye-ketiga',
-        type: 'iframe',
-        documents: [
-            { src: 'https://docs.google.com/presentation/d/e/2PACX-1vQbVCJpzI2nUbZFRprQOxS_FpNQsb_MKIUZA5iFgJzEsJfLqHfRF2f8i7hop8Vi4A/pubembed?start=true&loop=true&delayms=3000' }
-        ]
-
-    },
-    {
-        id: 'eye-keempat',
-        type: 'iframe',
-        documents: [
-            { src: 'https://drive.google.com/file/d/125yam0QpIghaPTiotOoI3e66QkW8-xwr/preview' }
-        ]
-
-    },
-    {
-        id: 'eye-kelima',
-        type: 'list',
-        documents: [
-            { 
-                judul: 'Analisis Dilema Etika.', 
-                href: 'https://drive.google.com/file/d/1iy2IP1CUVNrEq4NZW7EOJ4ZwKZEyH5AN/preview' 
-            },
-            { 
-                judul: 'Apakah whistleblowing atau tindakan melaporkan atau membocorkan dugaan pelanggaran atau kecurangan yang terjadi di dalam suatu organisasi di lingkungan kerja merupakan tindakan etis.', 
-                href: 'https://docs.google.com/presentation/d/e/2PACX-1vTDeynEODbSEQwAS2qAfudO92TR7wcyMjx-Qh3-oJslq2hCmLz6pPM9JdAkN7JjyQ/pubembed?start=true&loop=true&delayms=3000' 
-            }
-        ]
-    },
-    {
-        id: 'eye-keenam',
-        type: 'list',
-        documents: [
-            { 
-                judul: 'Analisis Kasus Etika Teknologi.', 
-                href: 'https://drive.google.com/file/d/1b4RQK8VGJ_1ddovVDtbie6pjGR1URmRe/preview' 
-            },
-            { 
-                judul: 'Apakah keamanan siber merupakan tanggung jawab moral atau teknis? Mahasiswa membuat argumentasi berdasarkan keempat prinsip etika TI.', 
-                href: 'https://drive.google.com/file/d/1j49yi4k1RKmj9aI9sh_esIrmf6HtEAI6/preview' 
-            }
-        ]
-    },
-    {
-        id: 'eye-ketujuh',
-        type: 'iframe',
-        documents: [
-            { src: 'https://drive.google.com/file/d/1JmnNDh3sw_EaYJb3Jznb3eFMSf6b0nfv/preview' }
-        ]
-    },
-    {
-        id: 'eye-kedelapan',
-        type: 'iframe',
-        documents: [
-            { src: 'https://drive.google.com/file/d/1AuLwAHyc3vzCkHwd1676_Nxqk5vRTlZT/preview' }
-        ]
-    },
-    {
-        id: 'eye-kesembilan',
-        type: 'list',
-        documents: [
-            { 
-                judul: 'Analisis Kasus Korupsi Digital.', 
-                href: 'https://drive.google.com/file/d/1ZB0iu0Y8fUF34HMqLsWm9J-aAbK8qTEK/preview' 
-            },
-            { 
-                judul: 'Tugas Kelompok: Simulasi Tender Etis.', 
-                href: '' 
-            }
-        ]
-    },
-];
-
-function createContainer(data) {
-    const container = document.getElementById('container-self-paced');
-    const divContainer = document.createElement('div');
-    divContainer.classList.add('container-popup');
-    divContainer.id = 'container-popup';
-    divContainer.style.display = 'flex';
-    const divContent = document.createElement('div');
-    divContent.classList.add('content-popup');
-    if (data.type === 'iframe' && data.documents.length === 1) {
-        const iframe = document.createElement('iframe');
-        iframe.src = data.documents[0].src;
-        iframe.width = '100%';
-        iframe.height = '800px';
-        divContent.appendChild(iframe);
+function startGreetings() {
+    const options = { timeZone: "Asia/Jakarta", hour: "2-digit", hour12: false };
+    const hourWIB = parseInt(new Intl.DateTimeFormat("id-ID", options).format(new Date()));
+    if (hourWIB >= 5 && hourWIB < 11) {
+        return "Selamat pagi";
+    } else if (hourWIB >= 11 && hourWIB < 15) {
+        return "Selamat siang";
+    } else if (hourWIB >= 15 && hourWIB < 18) {
+        return "Selamat sore";
+    } else {
+        return "Selamat malam";
     }
-    else if (data.type === 'list' || data.documents.length > 1) {
-        const divList = document.createElement('div');
-        divList.classList.add('eye-content');
-
-        data.documents.forEach(doc => {
-            const divDoc = document.createElement('div');
-            const h3 = document.createElement('h3');
-            h3.textContent = doc.judul;
-            const a = document.createElement('a');
-            a.href = doc.href;
-            a.target = '_blank';
-            a.textContent = 'Lihat';
-            divDoc.append(h3, a);
-            divList.appendChild(divDoc);
-        });
-        divContent.appendChild(divList);
-    }
-    const button = document.createElement('button');
-    button.type = 'reset';
-    button.textContent = 'Tutup';
-    button.id = 'close-btn';
-    divContent.appendChild(button);
-    divContainer.appendChild(divContent);
-    container.appendChild(divContainer);
-    closePopup();
-}
-
-function showDocument() {
-    const allEye = document.querySelectorAll('.eye-icon');
-    allEye.forEach(eye => {
-        eye.addEventListener('click', function(e) {
-            const target = e.target.id;
-            const selected = popupData.find(item => item.id === target);
-            if (selected) {
-                createContainer(selected);
-            }
-        })
-    })
-}
-
-function closePopup() {
-    const button = document.getElementById('close-btn');
-    if(button) {
-        button.addEventListener('click', function() {
-            const popup = document.getElementById('container-popup');
-            if(popup) {
-                popup.style.display = 'none';
-                setTimeout(() => {
-                    popup.remove();
-                }, 500);
-            }
-        })
-    }
-}
+};
 
 function showLogin() {
-    const body = document.querySelector('body');
     const divContainer = document.createElement('div');
     divContainer.setAttribute('id', 'login-container');
     const divContent = document.createElement('div');
     divContent.setAttribute('id', 'login-content');
+    const h1Greetings = document.createElement('h1');
+    h1Greetings.textContent = `${startGreetings()}.`;
     const pTitleContent = document.createElement('p');
     pTitleContent.textContent = 'Blog ini hanya dapat dilihat oleh Bapak Rahmadani, S.Kom., M.Kom.';
     const label = document.createElement('label');
+    label.htmlFor = 'password';
     label.textContent = 'Password';
     const input = document.createElement('input');
     input.setAttribute('id', 'password');
     input.type = 'password';
     input.minLength = '1';
     input.maxLength = '12';
-    input.name = 'password';
     const button = document.createElement('button');
     button.setAttribute('id', 'login-button');
     button.type = 'submit';
     button.textContent = 'Login';
-    divContent.append(pTitleContent, label, input, button);
+    divContent.append(h1Greetings, pTitleContent, label, input, button);
     divContainer.appendChild(divContent);
-    body.appendChild(divContainer);
+    document.body.appendChild(divContainer);
     showLoadLogin();
-}
+};
 
 function showLoadLogin() {
     const button = document.getElementById('login-button');
@@ -293,24 +65,189 @@ function showLoadLogin() {
             return;
         }
         error.remove();
-        showLayout();
+        showAllLayout();
         container.remove();
-    })
-}
+    });
+};
 
-function showFooter() {
-    const footer = document.getElementById('footer-container');
+function showAllLayout() {
+    const header = document.createElement('header');
+    const main = document.createElement('main');
+    const footer = document.createElement('footer');
+    document.body.append(header, main, footer);
+    loadHeader();
+    loadMain();
+    loadFooter();
+};
+
+function formatDate(date) {
+    const months = [
+        "Januari","Februari","Maret","April","Mei","Juni",
+        "Juli","Agustus","September","Oktober","November","Desember"
+    ];
+    const day = date.getDate();
+    const month = months[date.getMonth()];
+    const year = date.getFullYear();
+    return `${day} ${month} ${year}`;
+};
+
+function startClock() {
+    const pLeft = document.getElementById('wib-clock');
+    function updateTime() {
+        const options = {
+            timeZone: "Asia/Jakarta",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+        };
+        const timeWIB = new Intl.DateTimeFormat("id-ID", options).format(new Date());
+        pLeft.textContent = `Waktu: ${timeWIB}`;
+    }
+    updateTime();
+    setInterval(updateTime, 1000);
+};
+
+function loadHeader() {
+    const header = document.querySelector('header');
+    // Left
+    const divLeft = document.createElement('div');
+    const h1 = document.createElement('h1');
+    h1.textContent = 'Blog Pendidikan Anti Korupsi dan Etika Teknologi';
+    divLeft.appendChild(h1);
+    // Right
+    const divRight = document.createElement('div');
+    const pDate = document.createElement('p');
+    pDate.textContent = `${formatDate(new Date())}`;
+    const pClock = document.createElement('p');
+    pClock.setAttribute('id', 'wib-clock');
+    divRight.append(pDate, pClock);
+    header.append(divLeft, divRight);
+    startClock();
+};
+
+const data = [
+    { 
+        'title': 'Pertemuan 1', 
+        'text': 'Bagaimana Anda memaknai kejujuran dan tanggung jawab dalam penggunaan teknologi digital di kehidupan sehari-hari ?.',
+        'src': 'https://drive.google.com/file/d/1qx4orcWGXiYoDlBIYE8zP5uUF4OvLPfo/preview'
+    },
+    { 
+        'title': 'Pertemuan 2',
+        'text': 'Analisis Etika dan Teknologi dalam kasus Korupsi Digital di Indonesia.',
+        'src': 'https://drive.google.com/file/d/1Cs4P_d1Glc6gFCdac67SbB3iJ5Z3cnsh/preview'
+
+    },
+    { 
+        'title': 'Pertemuan 3',
+        'text': 'Merancang Pedoman Etika Digital di Lingkungan Kampus.',
+        'src': 'https://docs.google.com/presentation/d/e/2PACX-1vQbVCJpzI2nUbZFRprQOxS_FpNQsb_MKIUZA5iFgJzEsJfLqHfRF2f8i7hop8Vi4A/pubembed?start=true&loop=true&delayms=3000'
+
+    },
+    { 
+        'title': 'Pertemuan 4',
+        'text': 'Analisis Yuridis Kasus Korupsi Digital di Indonesia.',
+        'src': 'https://drive.google.com/file/d/125yam0QpIghaPTiotOoI3e66QkW8-xwr/preview'
+
+    },
+    { 
+        'title': 'Pertemuan 5',
+        'text': 'Analisis Dilema Etika. [1]',
+        'src': 'https://drive.google.com/file/d/1iy2IP1CUVNrEq4NZW7EOJ4ZwKZEyH5AN/preview'
+
+    },
+    { 
+        'title': 'Pertemuan 5',
+        'text': 'Apakah whistleblowing atau tindakan melaporkan atau membocorkan dugaan pelanggaran atau kecurangan yang terjadi di dalam suatu organisasi di lingkungan kerja merupakan tindakan etis. [2]',
+        'src': 'https://docs.google.com/presentation/d/e/2PACX-1vTDeynEODbSEQwAS2qAfudO92TR7wcyMjx-Qh3-oJslq2hCmLz6pPM9JdAkN7JjyQ/pubembed?start=true&loop=true&delayms=3000'
+
+    },
+    { 
+        'title': 'Pertemuan 6',
+        'text': 'Analisis Kasus Etika Teknologi. [1]',
+        'src': 'https://drive.google.com/file/d/1b4RQK8VGJ_1ddovVDtbie6pjGR1URmRe/preview'
+
+    },
+    { 
+        'title': 'Pertemuan 6',
+        'text': 'Apakah keamanan siber merupakan tanggung jawab moral atau teknis ?. [2]',
+        'src': 'https://drive.google.com/file/d/1j49yi4k1RKmj9aI9sh_esIrmf6HtEAI6/preview'
+
+    },
+    { 
+        'title': 'Pertemuan 7',
+        'text': 'Menegakkan Integritas Digital di Era AI dan Disinformasi.',
+        'src': 'https://drive.google.com/file/d/1JmnNDh3sw_EaYJb3Jznb3eFMSf6b0nfv/preview'
+
+    },
+    { 
+        'title': 'Pertemuan 8',
+        'text': 'Ujian Tengah Semester.',
+        'src': 'https://drive.google.com/file/d/1AuLwAHyc3vzCkHwd1676_Nxqk5vRTlZT/preview'
+    },
+    { 
+        'title': 'Pertemuan 9',
+        'text': 'Analisis Kasus Korupsi Digital. [1]',
+        'src': 'https://drive.google.com/file/d/1ZB0iu0Y8fUF34HMqLsWm9J-aAbK8qTEK/preview'
+    },
+    { 
+        'title': 'Pertemuan 9',
+        'text': 'Mensimulasikan Proses Pengadaan yang Transparan dan Bebas Korupsi. [2]',
+        'src': 'https://drive.google.com/file/d/1FtVL03JVCJ52A26j05VfGK4lwjWED9EX/preview'
+    },
+    { 
+        'title': 'Pertemuan 10',
+        'text': 'AI yang Bias dan Privasi yang Hilang: Di Mana Letak Keadilan Digital ?. [1]',
+        'src': 'https://drive.google.com/file/d/1KM8X90Rs-en3gLxSA-qsRpWIRnipb3aR/preview'
+    },
+    { 
+        'title': 'Pertemuan 10',
+        'text': 'Mempresentasikan Argumen Etis, Sosial, dan Teknologis. [2]',
+        'src': 'https://drive.google.com/file/d/1zQGWG2dLzopQJ3cyqK7j1aA_2nmoyxSz/preview'
+    },
+];
+
+function loadMain() {
+    const main = document.querySelector('main');
+    const section = document.createElement('section');
+    data.forEach((list, no) => {
+        const article = document.createElement('article');
+        article.classList.add('container');
+        article.setAttribute('no', no);
+        const image = new Image();
+        image.src = 'logo_unpab_4x.png';
+        image.width = 769;
+        image.height = 775;
+        image.loading = 'lazy';
+        const h2 = document.createElement('h2');
+        h2.textContent = list.title;
+        const p = document.createElement('p');
+        p.textContent = list.text;
+        article.append(image, h2, p);
+        section.appendChild(article);
+    });
+    main.appendChild(section);
+    document.querySelectorAll('.container').forEach(btn => {
+        btn.addEventListener('click', function(e) {
+            const index = this.getAttribute('no');
+            const src = data[index].src;
+            window.open(src, '_blank', 'noopener,noreferrer');
+        });
+    });
+};
+
+function loadFooter() {
+    const footer = document.querySelector('footer');
     const p = document.createElement('p');
     p.textContent = 'Made with ❤️: Jeremi. (2025)';
     footer.appendChild(p);
-}
+};
 
 function changeCache() {
-    const version = '20251119';
+    const version = '20251204';
     document.querySelectorAll('link[rel="stylesheet"]').forEach(link => {
     link.href = link.href + '?v=' + version;
     });
     document.querySelectorAll('script[src]').forEach(script => {
     script.src = script.src + '?v=' + version;
     });
-}
+};
