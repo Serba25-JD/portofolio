ed36c9f9b5593929a194f2e691a643ac9bbb152f
Serba25-JD <hydebb.99@gmail.com>
2025-12-24 10:45:51 +0700

    update

--- proyek/blog/script.js
+++ proyek/blog/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    showLogin();
+    showAllLayout();
     changeCache();
 });
 
@@ -64,9 +64,13 @@ function showLoadLogin() {
             error.textContent = 'Password salah.';
             return;
         }
+        localStorage.setItem('login', 'true');
         error.remove();
-        showAllLayout();
         container.remove();
+        if(url) {
+            window.open(url, '_blank', 'noopener,noreferrer');
+            url = null;
+        };
     });
 };
 
@@ -126,86 +130,112 @@ function loadHeader() {
 };
 
 const data = [
-    { 
+    {
         'title': 'Pertemuan 1', 
         'text': 'Bagaimana Anda memaknai kejujuran dan tanggung jawab dalam penggunaan teknologi digital di kehidupan sehari-hari ?.',
         'src': 'https://drive.google.com/file/d/1qx4orcWGXiYoDlBIYE8zP5uUF4OvLPfo/preview'
     },
-    { 
+    {
         'title': 'Pertemuan 2',
         'text': 'Analisis Etika dan Teknologi dalam kasus Korupsi Digital di Indonesia.',
         'src': 'https://drive.google.com/file/d/1Cs4P_d1Glc6gFCdac67SbB3iJ5Z3cnsh/preview'
 
     },
-    { 
+    {
         'title': 'Pertemuan 3',
         'text': 'Merancang Pedoman Etika Digital di Lingkungan Kampus.',
         'src': 'https://docs.google.com/presentation/d/e/2PACX-1vQbVCJpzI2nUbZFRprQOxS_FpNQsb_MKIUZA5iFgJzEsJfLqHfRF2f8i7hop8Vi4A/pubembed?start=true&loop=true&delayms=3000'
 
     },
-    { 
+    {
         'title': 'Pertemuan 4',
         'text': 'Analisis Yuridis Kasus Korupsi Digital di Indonesia.',
         'src': 'https://drive.google.com/file/d/125yam0QpIghaPTiotOoI3e66QkW8-xwr/preview'
 
     },
-    { 
+    {
         'title': 'Pertemuan 5',
         'text': 'Analisis Dilema Etika. [1]',
         'src': 'https://drive.google.com/file/d/1iy2IP1CUVNrEq4NZW7EOJ4ZwKZEyH5AN/preview'
 
     },
-    { 
+    {
         'title': 'Pertemuan 5',
         'text': 'Apakah whistleblowing atau tindakan melaporkan atau membocorkan dugaan pelanggaran atau kecurangan yang terjadi di dalam suatu organisasi di lingkungan kerja merupakan tindakan etis. [2]',
         'src': 'https://docs.google.com/presentation/d/e/2PACX-1vTDeynEODbSEQwAS2qAfudO92TR7wcyMjx-Qh3-oJslq2hCmLz6pPM9JdAkN7JjyQ/pubembed?start=true&loop=true&delayms=3000'
 
     },
-    { 
+    {
         'title': 'Pertemuan 6',
         'text': 'Analisis Kasus Etika Teknologi. [1]',
         'src': 'https://drive.google.com/file/d/1b4RQK8VGJ_1ddovVDtbie6pjGR1URmRe/preview'
 
     },
-    { 
+    {
         'title': 'Pertemuan 6',
         'text': 'Apakah keamanan siber merupakan tanggung jawab moral atau teknis ?. [2]',
         'src': 'https://drive.google.com/file/d/1j49yi4k1RKmj9aI9sh_esIrmf6HtEAI6/preview'
 
     },
-    { 
+    {
         'title': 'Pertemuan 7',
         'text': 'Menegakkan Integritas Digital di Era AI dan Disinformasi.',
         'src': 'https://drive.google.com/file/d/1JmnNDh3sw_EaYJb3Jznb3eFMSf6b0nfv/preview'
 
     },
-    { 
+    {
         'title': 'Pertemuan 8',
         'text': 'Ujian Tengah Semester.',
         'src': 'https://drive.google.com/file/d/1AuLwAHyc3vzCkHwd1676_Nxqk5vRTlZT/preview'
     },
-    { 
+    {
         'title': 'Pertemuan 9',
         'text': 'Analisis Kasus Korupsi Digital. [1]',
         'src': 'https://drive.google.com/file/d/1ZB0iu0Y8fUF34HMqLsWm9J-aAbK8qTEK/preview'
     },
-    { 
+    {
         'title': 'Pertemuan 9',
         'text': 'Mensimulasikan Proses Pengadaan yang Transparan dan Bebas Korupsi. [2]',
         'src': 'https://drive.google.com/file/d/1FtVL03JVCJ52A26j05VfGK4lwjWED9EX/preview'
     },
-    { 
+    {
         'title': 'Pertemuan 10',
         'text': 'AI yang Bias dan Privasi yang Hilang: Di Mana Letak Keadilan Digital ?. [1]',
         'src': 'https://drive.google.com/file/d/1KM8X90Rs-en3gLxSA-qsRpWIRnipb3aR/preview'
     },
-    { 
+    {
         'title': 'Pertemuan 10',
         'text': 'Mempresentasikan Argumen Etis, Sosial, dan Teknologis. [2]',
         'src': 'https://drive.google.com/file/d/1zQGWG2dLzopQJ3cyqK7j1aA_2nmoyxSz/preview'
     },
+    {
+        'title': 'Pertemuan 11',
+        'text': 'Merancang Mockup Dashboard Digital untuk Memantau Proyek TI secara transparan.',
+        'src': '#'
+    },
+    {
+        'title': 'Pertemuan 12',
+        'text': 'Menganalisis Satu Kasus Nyata Pelanggaran Etika Profesional TI.',
+        'src': '#'
+    },
+    {
+        'title': 'Pertemuan 13',
+        'text': 'Rancang dan Presentasikan Program Insider Threat untuk Sebuah Organisasi/Pemerintah atau mungkin universitas.',
+        'src': 'https://drive.google.com/file/d/1TAqnWK7rEB2gq3JmgQoFQY9Qd2j9daXe/preview'
+    },
+    {
+        'title': 'Pertemuan 14',
+        'text': 'Keberanian Moral dalam Sistem Whistleblowing: Studi Kasus dan Refleksi Etika. [1]',
+        'src': '#'
+    },
+    {
+        'title': 'Pertemuan 14',
+        'text': 'Menganalisis Dilema Etika yang Muncul dengan Mengacu Pada Teori Etika. [2]',
+        'src': '#'
+    }
 ];
 
+let url = null;
 function loadMain() {
     const main = document.querySelector('main');
     const section = document.createElement('section');
@@ -230,7 +260,12 @@ function loadMain() {
         btn.addEventListener('click', function(e) {
             const index = this.getAttribute('no');
             const src = data[index].src;
-            window.open(src, '_blank', 'noopener,noreferrer');
+            if(localStorage.getItem('login') === 'true') {
+                window.open(src, '_blank', 'noopener,noreferrer');
+            } else {
+                showLogin();
+                url = src;
+            };
         });
     });
 };
